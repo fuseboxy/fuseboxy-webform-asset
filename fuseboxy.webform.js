448b93b7aaf4c054ee5e6aae07e71fb3b36ca252
henrygotmojo <henrygotmojo@gmail.com>
2021-07-08 15:56:49 +0800

    fix typo

--- fuseboxy.webform.js
+++ fuseboxy.webform.js
@@ -123,7 +123,7 @@ $(function(){
 
 
 	// ajax upload : remove button
-	$(document).on('click', '.webform-input-file.ready .btn-remove,.webform-inut-image.ready .btn-remove', function(evt){
+	$(document).on('click', '.webform-input-file.ready .btn-remove,.webform-input-image.ready .btn-remove', function(evt){
 		var $btnRemove = $(this);
 		var $container = $btnRemove.closest('.webform-input-file,.webform-input-image');
 		var $btnUpload = $container.find('.btn-upload');
