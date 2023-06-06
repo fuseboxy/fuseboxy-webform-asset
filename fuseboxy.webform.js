7012aff941995d4f5d6dae32d0b83d48aaf090e8
henrygotmojo <henrygotmojo@gmail.com>
2023-06-06 14:12:39 +0800

    prelim deprecate simple-ajax-uploader and apply bootstrap-extend ajax-submit

--- fuseboxy.webform.js
+++ fuseboxy.webform.js
@@ -1,3 +1,4 @@
+console.log('dev');
 $(function(){
 
 
@@ -207,106 +208,6 @@ $(function(){
 	});
 
 
-	// ajax uploader : init
-	$(document).on('mouseover focus', '.webform-input-file:has(.btn-upload):not(.ready),.webform-input-image:has(.btn-upload):not(.ready)', function(evt){
-		var $container = $(this);
-		var $containerInner = $container.find('label.form-control-file');
-		var $btnUpload = $container.find('.btn-upload');
-		var $btnRemove = $container.find('.btn-remove');
-		var $hiddenField = $container.find('input[name^=data]');
-		// create preview link (when necessary)
-		var $previewLink = $container.find('.preview-link');
-		if ( !$previewLink.length ) $previewLink = $('<a class="preview-link small" target="_blank"></a>').hide().appendTo($containerInner);
-		// create error box (when necessary)
-		var $err = $container.find('.form-text.text-danger');
-		if ( !$err.length ) $err = $('<small class="form-text text-danger"></small>').hide().appendTo($containerInner);
-		// create progress bar (when neccessary)
-		var $progress = $container.find('.progress');
-		if ( !$progress.length ) $progress = $('<div class="progress mt-4 mb-n4 mx-n4 rounded-0" style="height: 2px;"><div class="progress-bar"></div></div>').hide().appendTo($containerInner);
-		// init ajax uploader
-		var uploader = new ss.SimpleUpload({
-			//----- essential config -----
-			button: $btnUpload,
-			name: $btnUpload.attr('id'),
-			url: $btnUpload.attr('data-upload-handler'),
-			//----- optional config -----
-			progressUrl: $btnUpload.attr('data-upload-progress'),
-			multiple: false,
-			maxUploads: 1,
-			debug: true,
-			// number of KB (false for default)
-			// ===> javascript use KB for validation
-			// ===> server-side use byte for validation
-			maxSize: parseInt($btnUpload.attr('data-filesize'))/1024,
-			// allowed file types (false for default)
-			// ===> server will perform validation again
-			allowedExtensions: $btnUpload.attr('data-filetype').split(','),
-			// control what file to show when choosing files
-			hoverClass: 'btn-hover',
-			focusClass: 'active',
-			responseType: 'json',
-			// show progress bar
-			onSubmit: function(filename, extension, uploadBtn, fileSize) {
-				$progress.show();
-				this.setProgressContainer( $progress );
-				this.setProgressBar( $progress.find('.progress-bar') );
-				// toggle other elements
-				$err.hide().html('');
-				$previewLink.hide().html('');
-				// additional parameter pass to upload-hanlder
-				uploader._opts.data['originalName'] = encodeURI(filename);
-			},
-			// validate allowed extension
-			onExtError: function(filename, extension) {
-				var msg = $btnUpload.attr('data-filetype-error').replace('{FILE_TYPE}', $btnUpload.attr('data-filetype').toUpperCase());
-				$err.show().html(msg);
-			},
-			// validate file size
-			onSizeError: function(filename, fileSize) {
-				var msg = $btnUpload.attr('data-filesize-error').replace('{FILE_SIZE}', parseInt($btnUpload.attr('data-filesize'))/(1024*1024)+'MB');
-				$err.show().html(msg);
-			},
-			// show link of uploaded file
-			onComplete: function(filename, response, uploadBtn, fileSize) {
-				// when success
-				// ===> change button text
-				// ===> update hidden field (and trigger [onchange] event)
-				// ===> display preview link
-				// ===> display preview image (when necessary)
-				if ( response.success ) {
-					$btnUpload.html( $btnUpload.attr('data-button-alt-text') );
-					$btnRemove.show();
-					$hiddenField.val(response.fileUrl).trigger('change');
-					$previewLink.show().attr('href', response.fileUrl);
-					$previewLink.html( response.isWebImage ? '<img src="'+response.fileUrl+'" class="img-thumbnail d-block mt-2" alt="" />' : response.filename );
-				// when failure
-				// ===> show error message
-				} else $err.show().html( response.msg ? response.msg : response );
-			},
-			// server error
-			onError: function(filename, errorType, status, statusText, response, uploadBtn, fileSize) {
-				alert('Error occurred while uploading file.\nPlease see console log for server response.');
-			}
-		}); // new-SimpleUpload
-		// mark flag
-		$container.addClass('ready');
-	});
-
-
-	// ajax upload : remove button
-	$(document).on('click', '.webform-input-file.ready .btn-remove,.webform-input-image.ready .btn-remove', function(evt){
-		var $btnRemove = $(this);
-		var $container = $btnRemove.closest('.webform-input-file,.webform-input-image');
-		var $btnUpload = $container.find('.btn-upload');
-		var $hiddenField = $container.find('input');
-		var $previewLink = $container.find('.preview-link');
-		$btnRemove.hide();
-		$btnUpload.html( $btnUpload.attr('data-button-text') );
-		$hiddenField.val('').trigger('change');
-		$previewLink.attr('href', '').html('').hide();
-	});
-
-
 	// autosave : timer
 	$('.webform-autosave').each(function(){
 		var $container = $(this);
@@ -324,6 +225,53 @@ $(function(){
 			// mark flag
 			$container.addClass('ready');
 		}, 1000);
+	});
+
+
+	// ajax uploader : init
+	$(document).on('mouseover focus', '.webform-input-file:has(.btn-choose):not(.uploader-ready),.webform-input-image:has(.btn-choose):not(.uploader-ready)', function(evt){
+		// elements
+		var $container  = $(this);
+		var $field      = $container.find('[data-toggle=ajax-upload]');
+		var $chooseBtn  = $( $field.attr('data-choose-button') );
+		var $removeBtn  = $( $field.attr('data-remove-button') );
+		var $preview    = $( $field.attr('data-preview') );
+		// create hidden form
+		var ajaxFormID = $container.attr('id')+'-ajax-upload';
+		var $ajaxForm = $('<form><input type="file" name="file" /><button type="submit">Upload</button></form>').attr({
+			'id'              : ajaxFormID,
+			'action'          : $field.attr('data-form-action'),
+			'method'          : 'post',
+			'enctype'         : 'multipart/form-data',
+			'data-toggle'     : 'ajax-submit',
+			'data-target'     : $field.attr('data-target'),
+			'data-callback'   : "$('#"+ajaxFormID+"').remove();",
+			'data-transition' : 'none',
+		}).hide().appendTo('body');
+		// hidden file field
+		// ===> choose file & auto-submit
+		var $hiddenFileField = $ajaxForm.find('[type=file]');
+		$hiddenFileField.on('change', function(evt){
+			evt.preventDefault();
+			if ( $(this).val().length ) $ajaxForm.find(':submit').click();
+		});
+		// choose button
+		// ===> open file select dialog
+		$chooseBtn.on('click', function(evt){
+			evt.preventDefault();
+			$hiddenFileField.click();
+		}).removeClass('disabled');
+		// remove button
+		// ===> click to clear selected image
+		$removeBtn.on('click', function(evt){
+			evt.preventDefault();
+			$removeBtn.hide();
+			$chooseBtn.html( $chooseBtn.attr('data-button-text') );
+			$field.val('').trigger('change');
+			$preview.attr('href', '').html('').hide();
+		});
+		// mark complete
+		$container.addClass('uploader-ready');
 	});
 
 
